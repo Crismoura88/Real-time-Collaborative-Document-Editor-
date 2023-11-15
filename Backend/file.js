9d47f95f8a5ca6364cd80f942eda586894f17529
Crismoura88 <150434207+Crismoura88@users.noreply.github.com>
2023-11-15 12:19:24 +0300

    Add files via upload
    Project Back-end setup

---
+++ Backend/file.js
@@ -0,0 +1,17 @@
+//project schema file
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fileSchema = new Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  data_entry: {
+    type: Object,
+    required: true,
+  },
+}, { timestamps: true });
+
+const File = mongoose.model('File', fileSchema);
+module.exports = File;
