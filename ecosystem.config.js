9998bcaca62e1003b6ec2005c7682ab8139d34a4
Crismoura88 <150434207+Crismoura88@users.noreply.github.com>
2023-12-18 10:53:01 +0000

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    apps: [
+      {
+        name: 'frontend', // Replace with your app name
+        script: 'frontend/node_modules/react-scripts/scripts/start.js', // Adjust the path as needed
+        watch: true,
+        ignore_watch: ['node_modules'],
+        instances: 1,
+        exec_mode: 'fork',
+        cwd: 'frontend', // Set the working directory to your React app
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+      {
+        name: 'backend', // Replace with your server name
+        script: 'backend/server.js', // Adjust the path as needed
+        watch: true,
+        ignore_watch: ['node_modules'],
+        instances: 1,
+        exec_mode: 'fork',
+        cwd: 'backend', // Set the working directory to your Node.js server
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
